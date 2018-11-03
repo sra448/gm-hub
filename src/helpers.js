a59c9266c0191f210a9f69db9b6816b603349770
Flo <florian.unternaehrer@simplificator.com>
2018-11-03 11:55:32 +0100

    refactor useRecordList to use array internally
    and also take initialItems

--- src/helpers.js
+++ src/helpers.js
@@ -1,46 +1,38 @@
-import { useReducer } from "react";
-import { lensPath, set, values } from "ramda";
-
-const uuid = () =>
-  "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
-    const r = (Math.random() * 16) | 0;
-    const v = c == "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+import { useReducer } from 'react';
+import { concat, lensIndex, lensPath, set } from 'ramda';
 
 const itemsReducer = (items, action) => {
+  const id = items.indexOf(action.item);
+
   switch (action.type) {
-    case "add":
-      const uid = uuid();
-      return set(lensPath([uid]), { uid, ...action.values }, items);
+    case 'add':
+      return concat(items, [action.item]);
 
-    case "remove":
-      return set(lensPath([action.item.uid]), null, items);
+    case 'remove':
+      return set(lensIndex(id), null, items);
 
-    case "set":
-      return set(lensPath([action.item.uid, action.prop]), action.value, items);
+    case 'set':
+      return set(lensPath([id, action.prop]), action.value, items);
 
     default:
       return items;
   }
 };
 
-// TODO: add default items
-// TODO: don't rely on an object to ensure order
-export const useRecordList = () => {
-  const [items, dispatch] = useReducer(itemsReducer, {});
+export const useRecordList = initialItems => {
+  const [items, dispatch] = useReducer(itemsReducer, initialItems || []);
 
-  const addItem = values => {
-    dispatch({ type: "add", values });
+  const addItem = item => {
+    dispatch({ type: 'add', item });
   };
 
   const removeItem = item => {
-    dispatch({ type: "remove", item });
+    dispatch({ type: 'remove', item });
   };
 
   const setItem = (item, prop, value) => {
-    dispatch({ type: "set", item, prop, value });
+    dispatch({ type: 'set', item, prop, value });
   };
 
-  return [values(items).filter(x => x), addItem, setItem, removeItem];
+  return [items.filter(x => x), addItem, setItem, removeItem];
 };
