5a855ff705150105e2d6f9fae8f402f0193ae5d5
Flo <florian.unternaehrer@simplificator.com>
2018-11-03 11:17:19 +0100

    extract player component and enhance with material-ui

--- src/index.js
+++ src/index.js
@@ -1,53 +1,13 @@
-import React, { useState, useReducer } from "react";
+import React from "react";
 import { render } from "react-dom";
 
 import { useRecordList } from "./helpers";
+import Players from "./players";
 
 const defaultPlayer = {
   name: "foo",
   ac: "",
   hp: ""
-};
-
-const Player = ({ name, ac, hp, isExpanded, expand, onChange, onRemove }) => {
-  return !isExpanded ? (
-    <div onClick={expand}>
-      <div>{name}</div>
-      <div>{ac}</div>
-      <div>{hp}</div>
-    </div>
-  ) : (
-    <div>
-      <input value={name} onChange={e => onChange("name", e.target.value)} />
-      <input value={ac} onChange={e => onChange("ac", e.target.value)} />
-      <input value={hp} onChange={e => onChange("hp", e.target.value)} />
-      <button onClick={onRemove}>remove</button>
-    </div>
-  );
-};
-
-const Players = ({ players, onCreate, onChange, onRemove }) => {
-  const [expandedPlayerId, setExpandedPlayerId] = useState();
-
-  return (
-    <div>
-      <h2>Players</h2>
-      <ul>
-        {players.map(player => (
-          <li key={player.uid}>
-            <Player
-              {...player}
-              isExpanded={expandedPlayerId === player.uid}
-              expand={() => setExpandedPlayerId(player.uid)}
-              onChange={(prop, value) => onChange(player, prop, value)}
-              onRemove={() => onRemove(player)}
-            />
-          </li>
-        ))}
-      </ul>
-      <button onClick={onCreate}>add player</button>
-    </div>
-  );
 };
 
 const Main = () => {
---
+++ src/players.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const Player = props => {
+  const { name, ac, hp, expanded } = props;
+  const { toggleExpanded, onChange, onRemove } = props;
+
+  return (
+    <ExpansionPanel expanded={expanded} onChange={toggleExpanded}>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <div>{name}</div>
+        <div>{ac}</div>
+        <div>{hp}</div>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <input value={name} onChange={e => onChange("name", e.target.value)} />
+        <input value={ac} onChange={e => onChange("ac", e.target.value)} />
+        <input value={hp} onChange={e => onChange("hp", e.target.value)} />
+        <button onClick={onRemove}>remove</button>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  );
+};
+
+export default ({ players, onCreate, onChange, onRemove }) => {
+  const [expandedPlayerId, setExpandedPlayerId] = useState();
+
+  return (
+    <div>
+      <h2>Players</h2>
+      <div>
+        {players.map(player => (
+          <Player
+            key={player.uid}
+            {...player}
+            expanded={expandedPlayerId === player.uid}
+            toggleExpanded={() =>
+              setExpandedPlayerId(
+                player.uid !== expandedPlayerId ? player.uid : null
+              )
+            }
+            onChange={(prop, value) => onChange(player, prop, value)}
+            onRemove={() => onRemove(player)}
+          />
+        ))}
+      </div>
+      <button onClick={onCreate}>add player</button>
+    </div>
+  );
+};
