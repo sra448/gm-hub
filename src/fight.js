6ee200a10f79127cadf81229462efb125cd3b544
Flo <florian.unternaehrer@simplificator.com>
2018-11-08 22:17:11 +0100

    add damage and heal buttons to battlefield

--- src/fight.js
+++ src/fight.js
@@ -15,6 +15,7 @@ import {
 import Avatar from '@material-ui/core/Avatar';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -25,6 +26,9 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
+import WhatsHotIcon from '@material-ui/icons/WhatsHotTwoTone';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospitalTwoTone';
+import GpsFixedIcon from '@material-ui/icons/GpsFixedTwoTone';
 
 import { useRecordList } from './helpers';
 
@@ -160,14 +164,26 @@ const Initiative = ({ players, opponents, startFight }) => {
   );
 };
 
+const incrementBy = amount => x => (x || 0) + amount;
+const decrementBy = amount => x => Math.max(0, (x || 0) - amount);
+
 const BattleField = ({ fighters }) => {
-  const [chars, _add, _del, setChar] = useRecordList(fighters);
+  const [chars, _add, _del, _setChar, overChar] = useRecordList(fighters);
   const [currentCharId, setCurrentCharId] = useState(0);
   const [roundId, setRoundId] = useState(1);
   const charsInOrder = [
     ...drop(currentCharId, chars),
     ...take(currentCharId, chars),
   ];
+
+  const dealDamage = (dealer, receiver, amount) => {
+    overChar(receiver, over(lensProp('damageReceived'), incrementBy(amount)));
+    overChar(dealer, over(lensProp('damageDealt'), incrementBy(amount)));
+  };
+
+  const heal = (char, amount) => {
+    overChar(char, over(lensProp('damageReceived'), decrementBy(amount)));
+  };
 
   const onNext = () => {
     if (currentCharId + 1 === chars.length) setRoundId(roundId + 1);
@@ -179,24 +195,66 @@ const BattleField = ({ fighters }) => {
       <Typography variant="h6">Round {roundId}</Typography>
       <Paper>
         <List>
-          {charsInOrder.map(char => (
-            <Fragment key={char.id}>
-              <ListItem>
-                <ListItemIcon>
-                  <Avatar>{type(char.id) === 'String' ? 'P' : char.id}</Avatar>
-                </ListItemIcon>
-                <ListItemText>{char.name}</ListItemText>
-              </ListItem>
-              <Divider />
-            </Fragment>
-          ))}
+          <ListItem className="current-player">
+            <ListItemIcon>
+              <Avatar>
+                {type(charsInOrder[0].id) === 'String'
+                  ? 'P'
+                  : charsInOrder[0].id}
+              </Avatar>
+            </ListItemIcon>
+            <ListItemText>
+              {charsInOrder[0].name}, {charsInOrder[0].damageDealt}
+            </ListItemText>
+          </ListItem>
+          <Divider />
+          {drop(
+            1,
+            charsInOrder.map(char => (
+              <Fragment key={char.id}>
+                <ListItem>
+                  <ListItemIcon>
+                    <Avatar>
+                      {type(char.id) === 'String' ? 'P' : char.id}
+                    </Avatar>
+                  </ListItemIcon>
+                  <ListItemText>
+                    {char.name}, {char.damageReceived}
+                  </ListItemText>
+                  <ListItemSecondaryAction>
+                    <IconButton
+                      onClick={() => heal(char, 20)}
+                      aria-label="heal"
+                    >
+                      <LocalHospitalIcon />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => dealDamage(char, charsInOrder[0], 13)}
+                      aria-label="take damage"
+                    >
+                      <WhatsHotIcon />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => dealDamage(charsInOrder[0], char, 12)}
+                      aria-label="deal damage"
+                    >
+                      <GpsFixedIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              </Fragment>
+            )),
+          )}
         </List>
       </Paper>
-      <div className="big-padding center">
-        <Button variant="contained" color="secondary" onClick={onNext}>
-          Next
-        </Button>
-      </div>
+      <Button
+        variant="fab"
+        color="secondary"
+        className="floating-bottom-right"
+        onClick={onNext}
+      >
+        Next
+      </Button>
     </div>
   );
 };
